5e5a7a061efb0dc6d437dbf2d24ead31eb72be86
Jack Goers <jackgoers@gmail.com>
2022-03-03 13:14:41 -0600

    Initial deployment of react app

---
+++ coffee-hour/src/App.js
@@ -0,0 +1,18 @@
+import logo from './coffee.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Landing Page
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
