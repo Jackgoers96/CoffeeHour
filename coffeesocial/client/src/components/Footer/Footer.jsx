1fc4c6c6704edc2d40ddb696bb928c4acdea5100
Jackgoers96 <81663225+Jackgoers96@users.noreply.github.com>
2022-04-10 16:36:44 -0500

    Beginning server set up. Including a MongoDB auth and aiming to have user Auth soon

--- coffeesocial/client/src/components/Footer/Footer.jsx
+++ coffeesocial/client/src/components/Footer/Footer.jsx
@@ -6,11 +6,89 @@ import {
   Link,
   Redirect,
 } from "react-router-dom";
+import Button from "@mui/material/Button";
+import Badge from "@mui/material/Badge";
+import LoginIcon from "@mui/icons-material/Login";
+import HomeIcon from "@mui/icons-material/Home";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import RedditIcon from "@mui/icons-material/Reddit";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "./Footer.css";
 function Footer() {
   return (
-    <div>
-      <p>Footer Section Goes here</p>
-    </div>
+    <Route>
+      <div>
+        <div id="footerDiv">
+          <div class="footerIcons">
+            <Button
+              varient="containted"
+              target="_blank"
+              href="https://facebook.com"
+              color="primary"
+              class="socials"
+            >
+              <Badge>
+                <FacebookIcon />
+              </Badge>
+            </Button>
+          </div>
+          <div class="footerIcons">
+            <Button
+              varient="contained"
+              target="_blank"
+              href="https://instagram.com"
+              color="primary"
+              class="socials"
+            >
+              <Badge>
+                <InstagramIcon />
+              </Badge>
+            </Button>
+          </div>
+          <div class="footerIcons">
+            <Button
+              varient="containted"
+              target="_blank"
+              href="https://reddit.com"
+              color="primary"
+              class="socials"
+            >
+              <Badge>
+                <RedditIcon />
+              </Badge>
+            </Button>
+          </div>
+          <div class="footerIcons">
+            <Button
+              varient="contained"
+              target="_blank"
+              href="https://twitter.com"
+              color="primary"
+              class="socials"
+            >
+              <Badge>
+                <TwitterIcon />
+              </Badge>
+            </Button>
+          </div>
+          <div class="footerIcons">
+            <Button
+              varient="contained"
+              target="_blank"
+              href="https://linkedin.com"
+              color="primary"
+              class="socials"
+            >
+              <Badge>
+                <LinkedInIcon />
+              </Badge>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </Route>
   );
 }
 export default Footer;
--- coffeesocial/client/src/components/Header/Header.jsx
+++ coffeesocial/client/src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import ReactDom from "react-dom";
 import Button from "@mui/material/Button";
 import Badge from "@mui/material/Badge";
 import LoginIcon from "@mui/icons-material/Login";
+import HomeIcon from "@mui/icons-material/Home";
 import SignUp from "../SignUp/SignUp";
 import {
   BrowserRouter as Router,
@@ -15,13 +16,21 @@ import "./Header.css";
 function Header() {
   return (
     <Router>
-      <div>
-        <Button varient="containted" href="./Home">
-          Go Home
-        </Button>
-        <Badge id="loginBadge" color="primary">
-          <LoginIcon />
-        </Badge>
+      <div class="headerDiv">
+        <div>
+          <Button varient="containted" href="./Home">
+            <Badge id="HomeBadge" color="primary">
+              <HomeIcon />
+            </Badge>
+          </Button>
+        </div>
+        <div>
+          <Button varient="contained" href="./SignUp">
+            <Badge id="loginBadge" color="primary">
+              <LoginIcon />
+            </Badge>
+          </Button>
+        </div>
       </div>
     </Router>
   );
