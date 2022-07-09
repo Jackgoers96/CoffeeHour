e098ee818993be3bf19b63683223a50f612865ab
Jackgoers96 <81663225+Jackgoers96@users.noreply.github.com>
2022-07-09 09:43:27 -0500

    Adding Redux to the pile

--- coffeesocial/client/src/index.js
+++ coffeesocial/client/src/index.js
@@ -1,27 +1,22 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from "@apollo/client";
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App'
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 const container = document.getElementById('root');
 
 const root = ReactDOM.createRoot(container);
 
 root.render(
 
-  <App />
-
-
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
